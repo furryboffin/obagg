33ff045af599bcbb44caf4dd18a9f726bff1ca07
Jonathan Fulcher <jonathan@bity.com>
2022-08-12 16:51:14 +0200

    Update gRPC server and client to handle a simple test case

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,53 @@
+use tonic::{transport::Server, Request, Response, Status};
 
-fn main() {
-    println!("Hello, world!");
+use orderbook::orderbook_aggregator_server::{OrderbookAggregator, OrderbookAggregatorServer};
+use orderbook::{Empty, Level, Summary};
+
+pub mod orderbook {
+    tonic::include_proto!("orderbook");
+}
+#[derive(Debug, Default)]
+pub struct OrderbookAggregatorService {}
+
+#[tonic::async_trait]
+impl OrderbookAggregator for OrderbookAggregatorService {
+    async fn book_summary(
+        &self,
+        request: Request<Empty>
+    ) -> Result<Response<Summary>, Status> {
+        println!("goit a request: {:?}", request);
+
+        // let req = request.into_inner();
+        let reply = Summary {
+            spread: 10.0,
+            bids: vec![
+                Level {
+                    exchange: "binance".to_string(),
+                    price: 100.1,
+                    amount: 500.1,
+                }
+            ],
+            asks: vec![
+                Level {
+                    exchange: "binance".to_string(),
+                    price: 100.1,
+                    amount: 500.1,
+                }
+            ],
+        };
+
+        Ok(Response::new(reply))
+
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let orderbook_service = OrderbookAggregatorService::default();
+    Server::builder()
+        .add_service(OrderbookAggregatorServer::new(orderbook_service))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
